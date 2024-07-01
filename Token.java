13eab902c94cf4bc5b5448479cd4a6e491b8ea09
MartinWrighton <martin.wrighton@outlook.com>
2024-07-01 23:41:34 +0100

    simplified diagonal movement

--- Token.java
+++ Token.java
@@ -65,26 +65,19 @@ public class Token extends JComponent{
         double moveR = this.direction[1];
         double moveU = this.direction[2];
         double moveD = this.direction[3];
-        if (this.direction[0]+this.direction[2]>1){ //preventing diagonal movement from being faster
-            double both = Math.sqrt(this.direction[0]*this.direction[0]+this.direction[2]*this.direction[2]);
-            moveL = this.direction[0]/both;
-            moveU = this.direction[2]/both;
-        }
-        if (this.direction[0]+this.direction[3]>1){
-            double both = Math.sqrt(this.direction[0]*this.direction[0]+this.direction[3]*this.direction[3]);
-            moveL = this.direction[0]/both;
-            moveD = this.direction[3]/both;
-        }
-        if (this.direction[1]+this.direction[3]>1){
-            double both = Math.sqrt(this.direction[1]*this.direction[1]+this.direction[3]*this.direction[3]);
-            moveR = this.direction[1]/both;
-            moveD = this.direction[3]/both;
-        }
-        if (this.direction[2]+this.direction[1]>1){
-            double both = Math.sqrt(this.direction[1]*this.direction[1]+this.direction[2]*this.direction[2]);
-            moveU = this.direction[2]/both;
-            moveR = this.direction[1]/both;
-        }
+        //preventing diagonal movement from being faster
+        double dify = Math.abs(moveU-moveD);
+        double difx = Math.abs(moveL-moveR);
+        double both = Math.sqrt(difx*difx+dify*dify);
+        double movex = difx/both;
+        double movey = dify/both;
+        moveL = moveL*movex;
+        moveR = moveR*movex;
+        moveU = moveU*movey;
+        moveD = moveD*movey;
+
+        
+        
         this.move((moveR-moveL)*this.moveSpeed,(moveD-moveU)*this.moveSpeed);
     }
 
